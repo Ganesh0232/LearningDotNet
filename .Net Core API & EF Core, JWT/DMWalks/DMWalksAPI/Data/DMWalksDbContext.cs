971b68ac990be14f00309fb03942c8a6343fa5fe
Ganesh Kumar Marrapu <ganeshkumar.marrapu+123@gmail.com>
2023-04-19 12:39:08 +0530

    End of Section 2 .Net Core API & EF Core, JWT

--- .Net Core API & EF Core, JWT/DMWalks/DMWalksAPI/Data/DMWalksDbContext.cs
+++ .Net Core API & EF Core, JWT/DMWalks/DMWalksAPI/Data/DMWalksDbContext.cs
@@ -1,6 +1,18 @@
-﻿namespace DMWalksAPI.Data
+﻿using DMWalksAPI.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DMWalksAPI.Data
 {
-    public class DMWalksDbContext
+    public class DMWalksDbContext : DbContext
     {
+        public DMWalksDbContext( DbContextOptions dbContextOptions) :base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public  DbSet<Region> Regions { get; set; }
+
+        public DbSet<Walk> Walks { get; set; }  
     }
 }
