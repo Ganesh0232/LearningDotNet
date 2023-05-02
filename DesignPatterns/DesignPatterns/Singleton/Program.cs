e21381905dc181792ecdde09f5f436716eb13486
Ganesh Kumar Marrapu <ganeshkumar.marrapu+123@gmail.com>
2023-05-02 14:03:43 +0530

    Added Desing patterns
    Added DesignPatterns

---
+++ DesignPatterns/DesignPatterns/Singleton/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Singleton Design Pattern \n");
+
+            Singleton singleton =  Singleton.GetInstance;
+            singleton.SingletonMethod("it is Singletons first instance");
+            singleton.SingletonMethod("second Instance");
+        }
+    }
+}
